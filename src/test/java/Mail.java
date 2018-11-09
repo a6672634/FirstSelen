9b687225766a2980775e60efbc96f8a42472738b
qa <a6672634@yandex.ru>
2018-11-09 19:13:24 +0300

    Homework

--- src/test/java/Mail.java
+++ src/test/java/Mail.java
@@ -3,7 +3,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Mail {
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\path\\to\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "Z:\\QA 2-2018\\Каневский\\Селениум\\ueu\\uu\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("http://www.mail.ru");
         driver.navigate().to("http://www.mail.ru");
--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -3,7 +3,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Main {
     public static void main(String[] args) {
-        System.setProperty("webdriver.gecko.driver", "C:\\Program Files\\geckodriver\\geckodriver.exe");
+        System.setProperty("webdriver.gecko.driver", "Z:\\QA 2-2018\\Каневский\\Селениум\\to\\path\\geckodriver.exe");
         WebDriver driver = new FirefoxDriver();
         driver.get("http://www.google.com");
         driver.navigate().to("http://www.google.com");
